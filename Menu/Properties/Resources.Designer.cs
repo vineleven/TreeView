4994492606680d40f858a39531754b7d5f95f6f6
liudaiwen <liudaiwen@3gwin.cn>
2016-07-21 12:33:02 +0800

    update

--- Menu/Properties/Resources.Designer.cs
+++ Menu/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace TreeMenu.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Menu.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TreeMenu.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
